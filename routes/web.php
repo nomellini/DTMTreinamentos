721cc38cad8832c42b6be06a3b31bd6adf3974ec
Fernando Nomellini <fernando@datamace.com.br>
2022-08-23 16:50:45 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PlanController;
+use App\Http\Controllers\Site\LoginController;
+use App\Http\Controllers\Site\ContatoController;
+use App\Http\Controllers\Site\Treinamento\TurmasController;
+use App\Http\Controllers\Site\Treinamento\CalendarioController;
+use App\Http\Controllers\Site\Treinamento\ListaTreinamentoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +26,29 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/', function() {
+    return view('welcome');
+})->name('home');
+
+
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+
+Route::prefix('/admin')->group(function() {
+    Route::get('/plans', [PlanController::class, 'index'])->name('admin.planos');
+});
+
+Route::prefix('/treinamento')->group(function() {
+    Route::get('/calendario/{mes?}/{ano?}', [CalendarioController::class, 'index'])->name('site.treinamento.calendario');
+    Route::get('/turmas-disponiveis/{id}/{mes?}/{ano?}', [TurmasController::class, 'show'])->name('treinamento.turmas-disponiveis');
+    Route::get('/lista', [ListaTreinamentoController::class, 'index'])->name('site.treinamento.lista');
+});
+
+Route::get('/login', [LoginController::class, 'login'])->name('site.login');
+Route::post('/logout', [LoginController::class, 'logout'])->name('site.logout');
+Route::post('/login', [LoginController::class, 'auth'])->name('site.auth');
+
+
 
 require __DIR__.'/auth.php';
